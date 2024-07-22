74fa44c947dd8b362960730ab78072f1fca9a311
Nilanjan Chowdhury <78751630+Nil69420@users.noreply.github.com>
2024-07-22 10:24:30 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,17 @@
 #include <std_msgs/Float32.h>
 #include <tf/transform_datatypes.h>
 
-// Function to convert quaternion component to angle in degrees
+
 float quaternionComponentToDegrees(float component) {
-    // Convert quaternion component to angle in degrees
-    return acos(component) * 180.0 / M_PI; // Convert from radians to degrees
+    
+    return acos(component) * 180.0 / M_PI; 
 }
 
-// Callback function to process the received PoseStamped message
+
 void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg, ros::Publisher& pub) {
     float z_angle = quaternionComponentToDegrees(msg->pose.orientation.z);
     float w_angle = quaternionComponentToDegrees(msg->pose.orientation.w);
 
-    // Normalize the angles between 0 and 360 degrees
     if (z_angle < 0) {
         z_angle += 360.0;
     }
@@ -23,7 +22,7 @@ void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg, ros::Publishe
         w_angle += 360.0;
     }
 
-    // Create a new PoseStamped message to publish
+   
     geometry_msgs::PoseStamped output_msg;
     output_msg.header = msg->header;
     output_msg.pose.position = msg->pose.position;
@@ -32,13 +31,13 @@ void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg, ros::Publishe
     output_msg.pose.orientation.z = z_angle;
     output_msg.pose.orientation.w = w_angle;
 
-    // Log the position and angles for debugging
+  
     ROS_INFO("Received PoseStamped: [position: x: %f, y: %f, z: %f], [orientation: z: %f, w: %f], Converted angles: z: %f degrees, w: %f degrees",
              msg->pose.position.x, msg->pose.position.y, msg->pose.position.z,
              msg->pose.orientation.z, msg->pose.orientation.w,
              z_angle, w_angle);
 
-    // Publish the new message
+   
     pub.publish(output_msg);
 }
 
